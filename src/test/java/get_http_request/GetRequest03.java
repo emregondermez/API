28a79b60997fc655c706c972d2ce10d606f5b5e1
talfik2 <emregondermez@gmail.com>
2022-03-11 00:09:31 +0100

    10.03.2022

---
+++ src/test/java/get_http_request/GetRequest03.java
@@ -0,0 +1,31 @@
+package get_http_request;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GetRequest03 {
+
+
+    @Test
+    public void test03() {
+        String url ="https://restful-booker.herokuapp.com/booking/7";
+        Response response = given().when().get(url);
+
+        response.prettyPrint();
+
+      response.then().statusCode(200).contentType(ContentType.JSON);
+
+
+        response
+                .then()
+                .assertThat()
+                .body("firstname", Matchers.equalTo("Susan"),
+                "lastname",Matchers.equalTo("Smith")
+                        ,"bookingdates.checkin",Matchers.equalTo("2019-09-28") );
+
+    }
+}
